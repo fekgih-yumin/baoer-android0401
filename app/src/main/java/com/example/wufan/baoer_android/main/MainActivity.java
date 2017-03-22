a7b007cc311ea6fe57b1b27ee53ba56df43d5340
yumin <001fhd@163.com>
2017-03-22 15:25:29 +0800

    wrote contract

---
+++ app/src/main/java/com/example/wufan/baoer_android/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wufan.baoer_android.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wufan.baoer_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/wufan/baoer_android/main/More/MoreActivity.java
@@ -0,0 +1,15 @@
+package com.example.wufan.baoer_android.main.More;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wufan.baoer_android.R;
+
+public class MoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+    }
+}
